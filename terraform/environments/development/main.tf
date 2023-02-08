d7c05a5503d23efbe79d33b66f0ce297ac43e579
Andrius Ordojan <andrius.or@gmail.com>
2023-02-08 18:59:37 +0100

    f

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -12,4 +12,5 @@ module "containers" {
   location                    = var.location
   resource_group_name         = module.general.resource_group_name
   global_admin_user_principal = var.global_admin_user_principal
+  ci_service_principal_name   = var.ci_service_principal_name
 }
